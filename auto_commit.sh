24b850bcbaf34e535f1304393d23c23426f6abdc
Vrishab20 <vrishabdavey412@gmail.com>
2024-10-10 07:23:30 -0400

    ~^ Auto commit #1 with backdated timestamp

--- auto_commit.sh
+++ auto_commit.sh
@@ -8,7 +8,7 @@ if [[ -n $(git status -s) ]]; then
 
     for i in {0..4}; do
         # Generate a unique backdated timestamp for each commit
-        FIXED_DATE=$(date -d "2024-10-09 11:22:30 + $i minutes" +"%Y-%m-%dT%H:%M:%S")
+        FIXED_DATE=$(date -d "2024-10-10 11:22:30 + $i minutes" +"%Y-%m-%dT%H:%M:%S")
         
         GIT_COMMITTER_DATE="$FIXED_DATE" \
         git commit --date="$FIXED_DATE" -m " ~^ Auto commit #$((i+1)) with backdated timestamp"
